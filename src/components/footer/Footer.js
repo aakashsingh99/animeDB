a318775f56aa357d535c698c85246ddd5c9ef868
aakash <ak.zing007@gmail.com>
2021-12-26 19:52:47 +0530

    updated bg, footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,12 +3,18 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer-wrapper'>
-            <div className="github-link">
-                Github:
-            </div>
-            <div className="api-used">
-                API:
-            </div>
+            <a className="github-link" 
+                href='https://github.com/aakashsingh99/animeDB'
+                target='_blank' rel="noreferrer"
+            >
+                    Github ➦
+            </a>
+            <a className="api-used"
+                href='https://jikan.moe/'
+                target='_blank' rel='noreferrer'
+            >
+                API ➦
+            </a>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Search from '../components/searchBar/Search'
 import Results from './Results'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-wrapper'>
             <Search/>
             <Results/>
         </div>
